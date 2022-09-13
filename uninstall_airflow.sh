f9a8d8ba1873e7ba0ac155a66b0b1e671d5c32d6
liveget <liveget@users.noreply.github.com>
2022-09-13 03:16:54 +0000

    scale the scheduler

--- uninstall_airflow.sh
+++ uninstall_airflow.sh
@@ -4,3 +4,4 @@ source ./pv/delete_all_pvc.sh
 source ./pv/delete_all_pv.sh
 source ./pv/delete_all_secret.sh
 kubectl delete -f ./pv/storage_class.yaml
+#mysql -h 10.103.129.116 -u jwang -p < reset_db.sql
